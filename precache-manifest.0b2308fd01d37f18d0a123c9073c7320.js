d9b583a19e48a13cf0c50f7443c9e38dc49ab11f
Or Dotan <ordel23@gmail.com>
2020-06-20 15:49:07 +0300

    Deploy

---
+++ precache-manifest.0b2308fd01d37f18d0a123c9073c7320.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c01dd9a0a26ec13ff0f",
+    "url": "/miss-books-vue/css/about.66af7e8c.css"
+  },
+  {
+    "revision": "463e2555659deec0f1b8",
+    "url": "/miss-books-vue/css/app.0f7c9382.css"
+  },
+  {
+    "revision": "d836a64878a5c71a05395165aaa2c048",
+    "url": "/miss-books-vue/img/book.png"
+  },
+  {
+    "revision": "79a60b0eb038676e60ce019f14ba1268",
+    "url": "/miss-books-vue/img/books.79a60b0e.jpg"
+  },
+  {
+    "revision": "11dfdef99c667d9317b5464685aa49f0",
+    "url": "/miss-books-vue/img/sale.11dfdef9.png"
+  },
+  {
+    "revision": "68d78fd1b7f9d671ceb9e6ff7dbdd67f",
+    "url": "/miss-books-vue/index.html"
+  },
+  {
+    "revision": "5c01dd9a0a26ec13ff0f",
+    "url": "/miss-books-vue/js/about.822d97e2.js"
+  },
+  {
+    "revision": "463e2555659deec0f1b8",
+    "url": "/miss-books-vue/js/app.06d37143.js"
+  },
+  {
+    "revision": "3f0e9f1bf4f95e61e5db",
+    "url": "/miss-books-vue/js/chunk-vendors.2103a4ef.js"
+  },
+  {
+    "revision": "31cde321b9a73bcb2cf0196924a03f36",
+    "url": "/miss-books-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/miss-books-vue/robots.txt"
+  }
+]);
